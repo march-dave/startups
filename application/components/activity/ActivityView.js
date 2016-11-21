3cbe339da4178cf9ffc0456d0f5cc23de4c56fba
march-dave <ysangy@gmail.com>
2016-11-21 08:35:26 -0500

    ActivityView

---
+++ application/components/activity/ActivityView.js
@@ -0,0 +1,14 @@
+import React, { component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+
+class ActivityView extends Component {
+  render() {
+
+  }
+}
+
+export default ActivityView;
