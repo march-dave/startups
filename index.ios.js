8710ca992c87b697906dc9ebdb17dfac1a5f6e8c
march-dave <ysangy@gmail.com>
2016-11-23 06:16:49 -0500

    up to date

--- index.ios.js
+++ index.ios.js
@@ -7,33 +7,16 @@
 
 import React, { Component } from 'react';
 import {
-   AppRegistry,
-   Navigator
+  AppRegistry,
+  Navigator
 } from 'react-native';
 
 import Landing from './application/components/Landing';
-import Dashboard from './application/components/Dashboard';
-import { globals } from './application/styles';
 
-class startups extends Component {
-   render() {
+class assemblies extends Component {
+  render() {
     return (
-      <Navigator
-        style={globals.flex}
-        initialRoute={{ name: 'Landing' }}
-        renderScene={(route, navigator) => {
-          switch(route.name){
-            case 'Landing':
-              return (
-                <Landing navigator={navigator}/>
-            );
-            case 'Dashboard':
-              return (
-                <Dashboard navigator={navigator} />
-              )
-          }
-        }}
-      />
+      <Landing />
     );
   }
 }
