6249304d8e846670c8952ea64d2b03415de14efb
march-dave <ysangy@gmail.com>
2016-11-28 06:09:39 -0500

    Landing Page

--- index.ios.js
+++ index.ios.js
@@ -11,8 +11,10 @@ import {
 } from 'react-native';
 
 import Landing from './application/components/Landing';
+import Dashboard from './application/components/Dashboard';
+import { globals } from './application/styles';
 
-class assemblies extends Component {
+class startups extends Component {
   render() {
     return (
       <Navigator
