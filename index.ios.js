87b5b27e670fbc271216a763ca0324977568e683
march-dave <ysangy@gmail.com>
2016-11-29 18:32:04 -0500

    Loading Error before check-In

--- index.ios.js
+++ index.ios.js
@@ -10,31 +10,77 @@ import {
   Navigator
 } from 'react-native';
 
-import Landing from './application/components/Landing';
-import Dashboard from './application/components/Dashboard';
-import { globals } from './application/styles';
+import NavigationBar from 'react-native-navbar'
+
+// import Landing from './application/components/Landing';
+// import Dashboard from './application/components/Dashboard';
+// import { globals } from './application/styles';
 
 class startups extends Component {
   render() {
     return (
-      <Navigator
-        style={globals.flex}
-        initialRoute={{ name: 'Landing' }}
-        renderScene={(route, navigator) => {
-          switch(route.name){
-            case 'Landing':
-              return (
-                <Landing navigator={navigator}/>
-            );
-            case 'Dashboard':
-              return (
-                <Dashboard navigator={navigator}/>
-            );
-          }
-        }}
-      />
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+       <NavigationBar
+         title={{ title: 'Title', tintColor: 'black', }}
+         leftButton={{ title: 'Back', }}
+         rightButton={{ title: 'Forward', }}
+         style={{ backgroundColor: "white", }}
+         statusBar={{ tintColor: "white", }}
+       />
+
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+//   render() {
+//     return (
+//       <Navigator
+//         style={globals.flex}
+//         initialRoute={{ name: 'Landing' }}
+//         renderScene={(route, navigator) => {
+//           switch(route.name){
+//             case 'Landing':
+//               return (
+//                 <Landing navigator={navigator}/>
+//             );
+//             case 'Dashboard':
+//               return (
+//                 <Dashboard navigator={navigator}/>
+//             );
+//           }
+//         }}
+//       />
+//     );
+//   }
+// }
 
 AppRegistry.registerComponent('startups', () => startups);
